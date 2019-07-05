a57b10e2a1332a626c71f9d58e4d9073e2451c97
Aren Gariki Zohranyan <arenz@rambler.ruz>
2019-07-05 16:44:04 +0400

    05.07.2019New

---
+++ ChatHubWPF/ChatHubWPF/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace ChatHubWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void RadButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your username is: " + TextBox1.Text + ", and password is: " + PasswordBox1.Password, "Congratulations!!!", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.Yes, MessageBoxOptions.DefaultDesktopOnly);
+
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                Window1 ProgramWindow = new Window1(TextBox1.Text);
+                ProgramWindow.Show();
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                con.Open();
+                command.CommandText = "insert into [Table](Name,Username) values('" + TextBox1.Text + "','" + TextBox1.Text + "')";
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            this.Close();
+        }
+
+        private void RadButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You are registered.", "Congratulations!!!", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.Yes, MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void TextBox1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox1.Text == "Username") TextBox1.Text = "";
+        }
+
+        private void TextBox1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox1.Text == "") TextBox1.Text = "Username";
+        }
+
+        private void PasswordBox1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox1.Password == "password") PasswordBox1.Password = "";
+        }
+
+        private void PasswordBox1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox1.Password == "") PasswordBox1.Password = "password";
+        }
+
+        private void TextBox2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox2.Text == "Name") TextBox2.Text = "";
+        }
+
+        private void TextBox2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox2.Text == "") TextBox2.Text = "Name";
+        }
+
+        private void TextBox3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox3.Text == "Email") TextBox3.Text = "";
+        }
+
+        private void TextBox3_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox3.Text == "") TextBox3.Text = "Email";
+        }
+
+        private void TextBox4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox4.Text == "Username") TextBox4.Text = "";
+        }
+
+        private void TextBox4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox4.Text == "") TextBox4.Text = "Username";
+        }
+
+        private void PasswordBox2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox2.Password == "password") PasswordBox2.Password = "";
+        }
+
+        private void PasswordBox2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox2.Password == "") PasswordBox2.Password = "password";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //RaiseEvent(new RoutedEventArgs());
+        }
+    }
+}
---
+++ ChatHubWPF/ChatHubWPF/Window1.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Entity;
+using ChatHubWPF.ServiceChat;
+using System.Threading;
+
+namespace ChatHubWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window, ServiceChat.IServiceChatCallback
+    {
+        public string username;
+        int id;
+        ServiceChat.ServiceChatClient client;
+        public Window1(string t1Text)
+        {
+            InitializeComponent();
+            username = t1Text;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+            id = client.Connect(username);
+            MessageBox.Show("Welcome, " + username + ", you are online now. Enjoy it.", "Congratulations!!!");
+            UpdateRadGridViewData();
+        }
+        public void MsgCallback(string msg)
+        {
+            ChatMessages.Items.Add(msg);
+            UpdateRadGridViewData();
+        }
+
+        public void UpdateRadGridViewData()
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                SqlCommand command = new SqlCommand("select Name,Username from [Table]", con);
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable("UsersTable");
+                dataAdapter.Fill(dataTable);
+                RadGridView.ItemsSource = dataTable;
+            }
+        }
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("delete from [Table] where Name='" + username + "'", con);
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            client.Disconnect(id);
+            Environment.Exit(0);
+        }
+        private void ButtonMinimize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateRadGridViewData();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            client.Disconnect(id);
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("delete from [Table] where Name='"+username+"'", con);
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                client.SendMsg(MessageTextbox.Text, id);
+                MessageTextbox.Text = "";
+            }
+        }
+    }
+}
