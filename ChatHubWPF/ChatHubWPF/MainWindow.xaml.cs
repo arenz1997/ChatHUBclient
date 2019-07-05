1a33e077b475d8519c5aeda34676a5a0bee0843b
Aren Gariki Zohranyan <arenz@rambler.ruz>
2019-07-05 16:13:14 +0400

    Updated05.07.2019

--- ChatHubWPF/ChatHubWPF/MainWindow.xaml.cs
+++ ChatHubWPF/ChatHubWPF/MainWindow.xaml.cs
@@ -35,24 +35,88 @@ namespace ChatHubWPF
         {
             MessageBox.Show("Your username is: " + TextBox1.Text + ", and password is: " + PasswordBox1.Password, "Congratulations!!!", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.Yes, MessageBoxOptions.DefaultDesktopOnly);
 
-            //using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
-            //{
-            //    SqlCommand command = new SqlCommand();
-            //    command.Connection = con;
-            //    con.Open();
-            //    command.CommandText = "insert into [Table](Name,Username) values('" + TextBox1.Text + "','" + TextBox1.Text + "')";
-            //    command.ExecuteNonQuery();
-            //    con.Close();
-            //}
-
-            Window1 ProgramWindow = new Window1(TextBox1.Text);
-            ProgramWindow.Show();
-            this.Hide();
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                Window1 ProgramWindow = new Window1(TextBox1.Text);
+                ProgramWindow.Show();
+                SqlCommand command = new SqlCommand();
+                command.Connection = con;
+                con.Open();
+                command.CommandText = "insert into [Table](Name,Username) values('" + TextBox1.Text + "','" + TextBox1.Text + "')";
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            this.Close();
         }
 
         private void RadButton_Click_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("You are registered.", "Congratulations!!!", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.Yes, MessageBoxOptions.DefaultDesktopOnly);
+        }
+
+        private void TextBox1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox1.Text == "Username") TextBox1.Text = "";
+        }
+
+        private void TextBox1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox1.Text == "") TextBox1.Text = "Username";
+        }
+
+        private void PasswordBox1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox1.Password == "password") PasswordBox1.Password = "";
+        }
+
+        private void PasswordBox1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox1.Password == "") PasswordBox1.Password = "password";
+        }
+
+        private void TextBox2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox2.Text == "Name") TextBox2.Text = "";
+        }
+
+        private void TextBox2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox2.Text == "") TextBox2.Text = "Name";
+        }
+
+        private void TextBox3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox3.Text == "Email") TextBox3.Text = "";
+        }
+
+        private void TextBox3_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox3.Text == "") TextBox3.Text = "Email";
+        }
+
+        private void TextBox4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (TextBox4.Text == "Username") TextBox4.Text = "";
+        }
+
+        private void TextBox4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (TextBox4.Text == "") TextBox4.Text = "Username";
+        }
+
+        private void PasswordBox2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox2.Password == "password") PasswordBox2.Password = "";
+        }
+
+        private void PasswordBox2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (PasswordBox2.Password == "") PasswordBox2.Password = "password";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //RaiseEvent(new RoutedEventArgs());
         }
     }
 }
--- ChatHubWPF/ChatHubWPF/Window1.xaml.cs
+++ ChatHubWPF/ChatHubWPF/Window1.xaml.cs
@@ -15,22 +15,38 @@ using Telerik.Windows.Controls;
 using System.Data.SqlClient;
 using System.Data;
 using System.Data.Entity;
+using ChatHubWPF.ServiceChat;
+using System.Threading;
 
 namespace ChatHubWPF
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Window1 : Window, ServiceChat.IServiceChatCallback
     {
-        string t1;
+        public string username;
+        int id;
+        ServiceChat.ServiceChatClient client;
         public Window1(string t1Text)
         {
             InitializeComponent();
-            t1 = t1Text;
+            username = t1Text;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client = new ServiceChatClient(new System.ServiceModel.InstanceContext(this));
+            id = client.Connect(username);
+            MessageBox.Show("Welcome, " + username + ", you are online now. Enjoy it.", "Congratulations!!!");
+            UpdateRadGridViewData();
+        }
+        public void MsgCallback(string msg)
+        {
+            ChatMessages.Items.Add(msg);
+            UpdateRadGridViewData();
         }
 
-        private void UpdateRadGridViewData()
+        public void UpdateRadGridViewData()
         {
             using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
             {
@@ -43,6 +59,14 @@ namespace ChatHubWPF
         }
         private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("delete from [Table] where Name='" + username + "'", con);
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            client.Disconnect(id);
             Environment.Exit(0);
         }
         private void ButtonMinimize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -50,15 +74,30 @@ namespace ChatHubWPF
             WindowState = WindowState.Minimized;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Welcome, " + t1 + ", you are online now. Enjoy it.", "Congratulations!!!");
-            //UpdateRadGridViewData();           
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             UpdateRadGridViewData();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            client.Disconnect(id);
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arenz\source\repos\ChatHubWPF\ChatHubWPF\Data\Users.mdf;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("delete from [Table] where Name='"+username+"'", con);
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                client.SendMsg(MessageTextbox.Text, id);
+                MessageTextbox.Text = "";
+            }
         }
     }
 }
